e2f937bfcd05d4b7f063be0c5fcfd1b7b7d5e6a6
FabianM <luiisfabian96@gmail.com>
2024-06-02 01:35:19 -0500

    implementacion del listview.builder y listTile en el HomePage

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +7,48 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("FLUTTER + MATERIAL 3"), centerTitle: true,),
-    );
+        appBar: AppBar(
+          title: Text("FLUTTER + MATERIAL 3"),
+          centerTitle: true,
+        ),
+        body: const _HomeView());
+  }
+}
 
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon , color: colors.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_outlined, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        //todo : Navegar a otra pantalla
+      },
+    );
   }
 }
