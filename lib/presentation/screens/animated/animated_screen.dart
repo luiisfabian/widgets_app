8cbe6f07e209cf648bb6f96eba15e0ecf7af5f8d
FabianM <luiisfabian96@gmail.com>
2024-06-04 09:38:37 -0500

    creando los archivos faltantes y agregacion a los archivos de barril

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnimatedScreen extends StatelessWidget {
+
+    static const String name = 'animated';
+
+  const AnimatedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfiteScrollScreen extends StatelessWidget {
+      static const String name = 'infiniteScroll';
+
+  const InfiteScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
