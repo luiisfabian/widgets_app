a5fd5edb85a82f5b6c1f8a70e54f417857ff10f0
FabianM <luiisfabian96@gmail.com>
2024-06-17 01:29:40 -0500

    arreglo del las imagenes por id y creacion del inidicador de carga

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -20,6 +20,7 @@ const appMenuItems = <MenuItem>[
   MenuItem(title: "SnackBars y dialogs", subTitle: "Indicadores en pantalla", link: "/snackbars", icon: Icons.info_outline),
   MenuItem(title: "Animated container", subTitle: "Animación de contenedor", link: "/animated", icon: Icons.check_box_outlined),
   MenuItem(title: "Checkbox, Radios y otros tiles", subTitle: "Ui Controls", link: "/uiControls", icon: Icons.keyboard_control_sharp),
-  MenuItem(title: "Page View Introduccion", subTitle: "Pequeño ttutorial", link: "/tutorial", icon: Icons.pages_outlined),
+  MenuItem(title: "Page View Introduccion", subTitle: "Pequeño tutorial", link: "/tutorial", icon: Icons.pages_outlined),
+  MenuItem(title: "Infinite Scroll", subTitle: "Muestra del infinite Scroll y pull refresh", link: "/infiniteScroll", icon: Icons.pages_outlined),
   
 ];
--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,12 +1,106 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiteScrollScreen extends StatelessWidget {
-      static const String name = 'infiniteScroll';
+class InfiteScrollScreen extends StatefulWidget {
+  static const String name = 'infiniteScroll';
 
   const InfiteScrollScreen({super.key});
 
   @override
+  State<InfiteScrollScreen> createState() => _InfiteScrollScreenState();
+}
+
+class _InfiteScrollScreenState extends State<InfiteScrollScreen> {
+  final ScrollController scrollController = new ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      if (((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent)) {
+        //load next Page
+        loadNextPage();
+      }
+      ;
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+  List<int> imagesIds = [1, 2, 3, 4, 5];
+
+  Future loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+    addFiveImages();
+    isLoading = false;
+
+    //todo revisar si esta montado el componente / widget
+    if (isMounted) return;
+    setState(() {});
+
+    //todo mover Scroll
+  }
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text("Infite Scrooll"),
+      ),
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          controller: scrollController,
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.contain,
+              placeholder: AssetImage('assets/images/jar-loading.gif'),
+              image: NetworkImage(
+                  "https://picsum.photos/id/${imagesIds[index]}/500/300"),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // child: const Icon(Icons.arrow_back_outlined),
+        child: !isLoading == true
+            ? SpinPerfect(
+                infinite: true,
+                child: FadeIn(
+                  child: const Icon(Icons.refresh_outlined),
+                ),
+              )
+            : const Icon(Icons.arrow_back_outlined),
+        onPressed: () => context.pop(),
+      ),
+    );
   }
 }
